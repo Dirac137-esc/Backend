7e0da72d38029d1952be30621794b5ec45c8f9a5
137 <159417724+Dirac137-esc@users.noreply.github.com>
2025-07-07 14:54:57 +0800

    /test endpoint added

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use('/foods', require('./routes/food.routes'));
 app.use('/menus', require('./routes/menu.routes'));
 app.use('/orders', require('./routes/order.routes'));
 app.use('/users', require('./routes/user.routes'));
-app.get('/test', (req, res) => res.json({ message: 'It works' }));
+app.get('/test', (req, res) => res.json({ message: 'Ажиллаж л байна даа' }));
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
